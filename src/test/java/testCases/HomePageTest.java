8e6baa46fe336aa30fa3977d6d5d5a2de240cd8f
Rubina Gill <msrubinagill@gmail.com>
2017-06-11 00:45:07 +0530

    updated assert messages

--- src/test/java/testCases/HomePageTest.java
+++ src/test/java/testCases/HomePageTest.java
@@ -11,6 +11,6 @@ public class HomePageTest extends SeleniumDriver {
 	public void launch() {
 		driver.navigate().to("http://en.wikipedia.org/wiki/Main_Page");
 		System.out.println(driver.getTitle());
-		Assert.assertTrue(false, "check");
+		Assert.assertTrue(false, "\ncheck\n\n\n");
 	}
 }
