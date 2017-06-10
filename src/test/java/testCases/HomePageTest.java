09cc75ebfd957a5d3106ed7c1234080d95ee1e37
Rubina Gill <msrubinagill@gmail.com>
2017-06-11 00:29:28 +0530

    checking messages for assert

--- src/test/java/testCases/HomePageTest.java
+++ src/test/java/testCases/HomePageTest.java
@@ -11,6 +11,6 @@ public class HomePageTest extends SeleniumDriver {
 	public void launch() {
 		driver.navigate().to("http://en.wikipedia.org/wiki/Main_Page");
 		System.out.println(driver.getTitle());
-		Assert.assertTrue(true, "check");
+		Assert.assertTrue(false, "check");
 	}
 }
